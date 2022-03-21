dfb59ca5fd8d76bfe2865e079e79b083cda7357e
FrancoisNeau <francoisneau.pro@gmail.com>
2022-03-21 16:47:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-nud1gx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-nud1gx)
---
+++ src/app/modules.constants.ts
@@ -0,0 +1,69 @@
+export const modules = [
+  {
+    name: 'Workforce',
+    children: ['Overview', 'Distribution', 'Trends', 'Waterfall'],
+  },
+  {
+    name: 'Structure',
+    children: [
+      'Overview',
+      'Reporting Layers',
+      'Span of Control',
+      'Organizational Efficiency',
+      'Organization Chart',
+    ],
+  },
+  {
+    name: 'Diversity & Inclusion',
+    children: ['Gender', 'Race & Ethnicity', 'Analysis'],
+  },
+  {
+    name: 'Hires',
+    children: ['Hires', 'Distribution', 'Failed Hires', 'Hiring Channels'],
+  },
+  {
+    name: 'Leavers',
+    children: ['Turnover', 'Turnover Insights', 'Flight Risk'],
+  },
+  {
+    name: 'Mobility',
+    children: [
+      'Internal Mobility',
+      'Promotions',
+      'Demotions',
+      'Lateral Moves',
+      'Build Buy Analysis',
+    ],
+  },
+  {
+    name: 'Rewards',
+    children: ['Overview'],
+  },
+  {
+    name: 'Performance & Potential',
+    children: ['Overview', 'Talent Pool', 'Talent Distribution'],
+  },
+  {
+    name: 'Succession',
+    children: [
+      'Overview',
+      'Plans',
+      'Network',
+      'Bench Strengh',
+      'Candidates',
+      'Mobility',
+    ],
+  },
+  {
+    name: 'Absenteeism',
+    children: ['Rate', 'Impact'],
+  },
+  {
+    name: 'Engagement',
+    children: ['Results', 'Text Analysis'],
+  },
+  {
+    name: 'Tooling',
+    children: ['Power Report', 'data Quality'],
+  },
+];
